a3b572c1f961b7e77c5f3c904a02671f03cd18b2
Nathan Russell <nathan@claritysoftware.co.uk>
2023-05-18 08:32:32 +0100

    Add flyway; add initial db DDL

---
+++ src/main/resources/db/migration/V2023_07_17_0001.sql
@@ -0,0 +1,16 @@
+CREATE TABLE one_time_code (
+    id VARCHAR(36) PRIMARY KEY,
+    one_time_code_id VARCHAR(36) NOT NULL,
+    `value` VARCHAR(255) NOT NULL,
+    expires TIMESTAMP(3) NOT NULL,
+    attempts SMALLINT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    date_created TIMESTAMP(3) NOT NULL,
+    date_updated TIMESTAMP(3) NOT NULL,
+    version SMALLINT NOT NULL
+);
+
+CREATE UNIQUE INDEX one_time_code_id_idx
+ON one_time_code (
+    one_time_code_id
+);
